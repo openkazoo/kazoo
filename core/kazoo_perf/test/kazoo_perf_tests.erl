ab97ebe6333b465c91abc7c54324693d551cd16d
Rob Nichols <robert.nichols@dialwave.com>
2026-02-21 22:08:31 -0500

    core/kazoo_perf: fix unused term warnings in tests
    Signed-off-by: Rob Nichols <robert.nichols@dialwave.com>

--- core/kazoo_perf/test/kazoo_perf_tests.erl
+++ core/kazoo_perf/test/kazoo_perf_tests.erl
@@ -9,13 +9,13 @@ metrics_test_() ->
         {A, C, Z} = {<<"my_account">>, <<"my_cluster">>, <<"my_zone">>},
 
         %% Ensure Graphite metrics call completes without crashing
-        ?_assertEqual(
+        ?assertEqual(
             no_return,
             kazoo_perf_maintenance:graphite_metrics(A, C, Z)
         ),
 
         %% Ensure JSON metrics call completes without crashing
-        ?_assertEqual(
+        ?assertEqual(
             no_return,
             kazoo_perf_maintenance:json_metrics()
         ),
